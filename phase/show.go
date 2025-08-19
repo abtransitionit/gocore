55ba169dd14f0e1e9be8785f97dffad94e7a66ff
abelgacem <amarbelgacem@hotmail.fr>
2025-08-19 13:34:17 +0200

    upd

--- phase/show.go
+++ phase/show.go
@@ -9,13 +9,14 @@ import (
 	"os"
 	"sort"
 
+	"github.com/abtransitionit/gocore/logx"
 	"github.com/jedib0t/go-pretty/v6/table"
 )
 
 // Name: Show
 //
 // Description: Displays a list of phases in a pretty, human-readable table.
-func (w *Workflow) Show() {
+func (w *Workflow) Show(l logx.Logger) {
 	// Create a new table writer
 	t := table.NewWriter()
 	t.SetOutputMirror(os.Stdout)
@@ -37,7 +38,8 @@ func (w *Workflow) Show() {
 		}
 		t.AppendRow(table.Row{phase.Name, phase.Description, deps})
 	}
-
+	l.Info("Available phases:")
 	// Render the table
 	t.Render()
+	fmt.Println() // Add a newline for better readability after the table.
 }
