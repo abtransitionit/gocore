3026dd94a0e1c411e7a7bcbbb7a03e82672ec7ee
abelgacem <amarbelgacem@hotmail.fr>
2025-08-20 09:22:12 +0200

    upd

--- list/list.go
+++ list/list.go
@@ -1,6 +1,8 @@
 // File to create in gocore/list/list.go
 package list
 
+import "sort"
+
 // # Purpose
 //
 // - get the list of a Map:key from a map
@@ -17,13 +19,15 @@ package list
 //
 // - This function accepts any kind of map
 func GetMapKeys[V any](m map[string]V) []string {
-
 	// defining size is more efficient
 	keys := make([]string, 0, len(m))
 
 	for key := range m {
 		keys = append(keys, key) // Add the current key to our slice.
 	}
+
+	// Use the sort package to order the keys alphabetically.
+	sort.Strings(keys)
 
 	return keys
 }
