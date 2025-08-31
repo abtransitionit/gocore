b633cfb05869f6318cfb916f2d2e924998d90b0b
abelgacem <amarbelgacem@hotmail.fr>
2025-08-31 14:38:39 +0200

    update

--- gocli/gocli.go
+++ gocli/gocli.go
@@ -7,15 +7,15 @@ import (
 	"github.com/abtransitionit/gocore/logx"
 )
 
-func Install(logger logx.Logger, cli GoCli, osType string, osArch string, uname string) (string, error) {
-	// resolve URL
-	url, err := ResolveURL(logger, cli, osType, osArch, uname)
-	if err != nil {
-		return "", err
-	}
-	logger.Infof("Cli: %s Url: %s", cli.Name, url)
-	return url, nil
-}
+// func Install(logger logx.Logger, cli GoCli, osType string, osArch string, uname string) (string, error) {
+// 	// resolve URL
+// 	url, err := ResolveURL(logger, cli, osType, osArch, uname)
+// 	if err != nil {
+// 		return "", err
+// 	}
+// 	logger.Infof("Cli: %s Url: %s", cli.Name, url)
+// 	return url, nil
+// }
 
 // Name:resolveURL
 //
@@ -35,13 +35,13 @@ func ResolveURL(logger logx.Logger, cli GoCli, osType string, osArch string, una
 	}
 	// For now, just use Version directly
 	tag := cli.Version
-	return substitutePlaceholders(template.Url, cli, tag, osType, osArch, uname), nil
+	return substituteUrlPlaceholders(template.Url, cli, tag, osType, osArch, uname), nil
 }
 
-// Name: substitutePlaceholders
+// Name: substituteUrlPlaceholders
 //
 // Description: replaces placeholders in the URL template.
-func substitutePlaceholders(templatedUrl string, cli GoCli, tag string, osType string, osArch string, uname string) string {
+func substituteUrlPlaceholders(templatedUrl string, cli GoCli, tag string, osType string, osArch string, uname string) string {
 
 	replacements := map[string]string{
 		"$NAME":  cli.Name,
--- gocli/tgz.go
+++
@@ -1,7 +0,0 @@
-package gocli
-
-import "fmt"
-
-func ManageTgz(filepath string) (string, error) {
-	return fmt.Sprintf("manage a Go Tgz file onto the same OS:FS:%s", filepath), nil
-}
