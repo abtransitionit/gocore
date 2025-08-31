d0c4c9c1b2e22e20072e943f461424630b961500
abelgacem <amarbelgacem@hotmail.fr>
2025-08-31 14:07:21 +0200

    update

--- gocli/tgz.go
+++ gocli/tgz.go
@@ -3,6 +3,5 @@ package gocli
 import "fmt"
 
 func ManageTgz(filepath string) (string, error) {
-	// manage a Go Tgz file onto the same OS:FS -  mean : more job: extract and then decide
 	return fmt.Sprintf("manage a Go Tgz file onto the same OS:FS:%s", filepath), nil
 }
