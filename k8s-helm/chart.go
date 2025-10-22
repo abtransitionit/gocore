402e73415227d38a5cb2b5350843c378ffa90229
abelgacem <amarbelgacem@hotmail.fr>
2025-10-22 16:27:15 +0200

    update

--- k8s-helm/chart.go
+++ k8s-helm/chart.go
@@ -28,11 +28,19 @@ func (chart HelmChart) List() (string, error) {
 
 // Return: The cli to list the kind of all templates a helm charts it will create
 // Todo: it is linux specific. should be in library:golinux not gocore. or change the code to be OS agnostic
-func (chart HelmChart) ListKind() (string, error) {
+func (chart HelmChart) ListNbKind() (string, error) {
 
 	var cmds = []string{
 		fmt.Sprintf(`echo -e "Kind\tCount" && helm template %s | grep '^kind:' | sort | uniq -c | sed 's/kind: *//' | awk '{printf "%%s\t%%s\n", $2, $1}'`, chart.FullName),
 		// fmt.Sprintf(`echo "COUNT  KIND" && helm template %s | grep '^kind:' | sort | uniq -c | sed 's/kind: *//' | awk '{print $1, $2":", $3}'`, chart.FullName),
+	}
+	cli := strings.Join(cmds, " && ")
+	return cli, nil
+}
+func (chart HelmChart) ListKind() (string, error) {
+
+	var cmds = []string{
+		fmt.Sprintf(`echo -e "Kind\tName" && helm template %s | grep -A2 '^kind:' | grep -A2 '^kind:' | egrep 'kind|name' | paste - - | sed 's/kind: *//' | sed 's/name: *//'`, chart.FullName),
 	}
 	cli := strings.Join(cmds, " && ")
 	return cli, nil
@@ -59,7 +67,26 @@ func ListChart(local bool, remoteHost string, repo HelmRepo, logger logx.Logger)
 }
 
 // Returns the list of all kind the chart will create
-func ListChartKind(local bool, remoteHost string, chart HelmChart, logger logx.Logger) (string, error) {
+func (chart HelmChart) ListNbChartKind(local bool, remoteHost string, logger logx.Logger) (string, error) {
+
+	// define cli
+	cli, err := chart.ListNbKind()
+	if err != nil {
+		return "", fmt.Errorf("failed to build cli: %w", err)
+	}
+
+	// play cli
+	output, err := run.ExecuteCliQuery(cli, logger, local, remoteHost, run.NoOpErrorHandler)
+	if err != nil {
+		return "", fmt.Errorf("failed to run command: %s: %w", cli, err)
+	}
+
+	// return response
+	return output, nil
+}
+
+// Returns the list of all kind the chart will create
+func (chart HelmChart) ListChartKind(local bool, remoteHost string, logger logx.Logger) (string, error) {
 
 	// define cli
 	cli, err := chart.ListKind()
