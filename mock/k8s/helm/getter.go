f47e392656f860cc9323665921b3dff1d57b51cb
abelgacem <amarbelgacem@hotmail.fr>
2026-01-05 14:13:27 +0100

    update

---
+++ mock/k8s/helm/getter.go
@@ -0,0 +1,17 @@
+package helm
+
+import (
+	"fmt"
+
+	"github.com/abtransitionit/gocore/filex"
+)
+
+func getYaml(hostName string) (*MapYaml, error) {
+	// 1 - get local auto cached (embedded) file into a struct
+	yamlAsStruct, err := filex.LoadYamlIntoStruct[MapYaml](yamlList)
+	if err != nil {
+		return nil, fmt.Errorf("%s > loading config: %w", hostName, err)
+	}
+	// handle success
+	return yamlAsStruct, nil
+}
